3f6a99b0165907a386abb46f982a437786fe9e21
wishSowon <wish98choi@gmail.com>
2020-10-02 11:01:10 +0900

    Updates

---
+++ precache-manifest.90a483a4820dca79cee9ac529183bd87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e54f373fc0af41edb17383e62ff203",
+    "url": "/movie_app_2020/sexyapp/index.html"
+  },
+  {
+    "revision": "d45303241fe4f28b9cb6",
+    "url": "/movie_app_2020/sexyapp/static/css/main.0a512809.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020/sexyapp/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/sexyapp/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45303241fe4f28b9cb6",
+    "url": "/movie_app_2020/sexyapp/static/js/main.e3d73c1c.chunk.js"
+  },
+  {
+    "revision": "06a818a165065a73f76c",
+    "url": "/movie_app_2020/sexyapp/static/js/runtime-main.cc97c779.js"
+  }
+]);
